6a44fc5b8bde06356c1e7d3163754f94d04023d2
DevHsu <dlsrks7676@gmail.com>
2024-12-12 17:07:54 +0900

    first commit

---
+++ EatPlatform/src/main/java/com/eatplatform/web/config/RootConfig.java
@@ -0,0 +1,58 @@
+package com.eatplatform.web.config;
+
+import javax.sql.DataSource;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+// root-context.xml과 동일
+@Configuration
+@ComponentScan(basePackages = {"com.eatplatform.web.service"})
+@MapperScan(basePackages = {"com.eatplatform.web.persistence"})
+@ComponentScan(basePackages = {"com.eatplatform.web.aspect"})
+@EnableAspectJAutoProxy
+// 패키지 경로로 Mapper 스캐닝
+public class RootConfig {
+	
+	@Bean // 스프링 bean으로 설정. xml의 <bean>태그와 동일, 빈은 DI에 속한다.
+	public DataSource dataSource() { // DataSource 객체 리턴 메소드
+		// HikariConfig : DBCP 라이브러리
+		// 연결 구조
+		// Oracle DB -> JDBC -> DBCP(HikariCP) -> SqlSessionFactory(MyBatis)
+		HikariConfig config = new HikariConfig(); // 설정 객체
+		config.setDriverClassName("oracle.jdbc.OracleDriver");
+		config.setJdbcUrl("jdbc:oracle:thin:@192.168.0.166:1521:xe");
+		config.setUsername("eatroot"); // DB 사용자 아이디
+		config.setPassword("1234"); // DB 사용자 비밀번호
+		
+		config.setMaximumPoolSize(10); // 최대 풀(Pool) 크기 설정
+		config.setConnectionTimeout(30000); // Connection 타임 아웃 설정(30초)
+		HikariDataSource ds = new HikariDataSource(config);
+		
+		return ds; // ds 객체 리턴
+	}
+	
+	@Bean
+	public SqlSessionFactory sqlSessionFactory() throws Exception {
+		SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+		sqlSessionFactoryBean.setDataSource(dataSource());
+		return (SqlSessionFactory) sqlSessionFactoryBean.getObject();
+	}
+	
+	// 트랜잭션 매니저 객체를 빈으로 등록
+	@Bean
+	public PlatformTransactionManager transactionManager() {
+		return new DataSourceTransactionManager(dataSource());
+	}
+	
+} // end RootConfig
---
+++ README.md
@@ -0,0 +1,1 @@
+# eatPlatform-springLegacy
